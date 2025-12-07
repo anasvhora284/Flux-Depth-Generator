394439a58f730b72b83fa9edef00ec5d8564216a
Anas Vhora <vhoraayaz8@gmail.com>
2025-12-07 13:56:04 +0530

    feat: Add Railway deployment guide to README and configure Next.js for standalone output.

--- Flux-Wallpaper-Web/frontend/next.config.ts
+++ Flux-Wallpaper-Web/frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
   allowedDevOrigins: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://10.121.223.22:3000', 'http://10.121.223.22:3000', '10.121.223.22'],
 };
 
